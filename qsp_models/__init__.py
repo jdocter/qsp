22bb95fe95040bc9bef7f8fc4547ec0cb5075b33
Jordan Docter <jesdocter@gmail.com>
2021-10-26 01:19:35 -0500

    bivariate training

--- qsp_models/__init__.py
+++ qsp_models/__init__.py
@@ -1,3 +1,4 @@
 from .qsp_layers import QSP, construct_qsp_model
+from .bi_qsp_layers import BQSP, construct_bqsp_model
 from .qsp_circuit import QSPCircuit
 from .viz_tools import plot_loss, plot_qsp_response
